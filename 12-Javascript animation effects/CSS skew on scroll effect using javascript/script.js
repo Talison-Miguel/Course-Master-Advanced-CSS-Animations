992a84515af90d1252b2957529c60997e9384056
Tálison Miguel <talisonmiguel84@gmail.com>
2024-04-02 09:09:01 -0300

    CSS skew on scroll effect using javascript

---
+++ 12-Javascript animation effects/CSS skew on scroll effect using javascript/script.js
@@ -0,0 +1,6 @@
+let skewed = document.querySelector('.skewed');
+
+window.addEventListener('scroll', () => {
+    let value = -10 + window.scrollY/60;
+    skewed.style.transform = `skewY(${value}deg)`
+})
